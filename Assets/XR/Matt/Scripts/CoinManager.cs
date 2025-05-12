7f6b9a344ca4e225fb22cac26a9edee2f331e502
BattlefieldGuy <matthewklijnstra@gmail.com>
2025-05-12 13:18:30 +0200

    coin manager start

---
+++ Assets/XR/Matt/Scripts/CoinManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+
+    public int AttackCoins { get; private set; }
+    public int DefencCoins { get; private set; }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void LoseAttackCoins(int _amount)
+    {
+
+    }
+
+    public void LoseDefendCoins(int _amount)
+    {
+
+    }
+}
