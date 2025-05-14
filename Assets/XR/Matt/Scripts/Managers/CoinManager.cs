29d22fb10d39758f950967bfd3a957c42a5c43d1
BattlefieldGuy <matthewklijnstra@gmail.com>
2025-05-14 13:13:55 +0200

    Coin manager finish
    Done:
     - final tweaks for functions
     - added comments

--- Assets/XR/Matt/Scripts/Managers/CoinManager.cs
+++ Assets/XR/Matt/Scripts/Managers/CoinManager.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class CoinManager : MonoBehaviour
 {
+    /// <summary>
+    /// In this script all things that involve coins are managed here
+    /// 
+    /// base functions are for both sides
+    /// 
+    /// Attackers side has passive, steal and reward gains
+    /// 
+    /// Defenders side gets rewarded for killing minions
+    /// 
+    ///  - Debug to be deleted before final build -
+    /// </summary>
 
     #region --- Variables ---
 
     //Base Vars
-
     public static CoinManager INSTANCE;
 
     private int DefendersCoins;
@@ -15,9 +25,6 @@ public class CoinManager : MonoBehaviour
 
     //Attackers side
     [Header("AttackersSide")]
-
-    static int STEALLEVEL = 1;
-    static bool isSTEAL = false;
 
     [SerializeField] private int passiveIncomeAmount;
     [SerializeField] private int passiveIncomeTime;
@@ -30,6 +37,8 @@ public class CoinManager : MonoBehaviour
 
     private float incomeTimer = 2;
 
+    private int stealLevel = 1;
+    private bool isSteal = false;
     private float stealTimer = 0;
 
     //Defenders side
@@ -52,20 +61,23 @@ public class CoinManager : MonoBehaviour
 #endif
     }
 
+    //basic buy functions
     static void LoseATCoins(int _amount) => INSTANCE.AttackersCoins -= _amount;
 
     static void LoseDECoins(int _amount) => INSTANCE.AttackersCoins -= _amount;
 
     #endregion
 
-    #region --- AT FUNCTIONS ---
+    //(attacker)
+    #region --- AT FUNCTIONS --- 
+
+    //function that is called once a tower is destroyed to calculate the reward
     public static void GainTowerPrize(int _towerLevel, int _towerPrize)
     {
         float _gain = 0;
-        _gain = _towerPrize * _towerLevel;
-        _gain *= INSTANCE.towerPrizeMultiplier;
-        int _amount = Mathf.RoundToInt(_gain);
-#if UNITY_EDITOR
+        _gain = _towerPrize * _towerLevel;//multiply by level to increase reward by status
+        int _amount = Mathf.RoundToInt(_gain * INSTANCE.towerPrizeMultiplier);//custum multiplier to tweak the final reward that is rounded to add to main counter
+#if UNITY_EDITOR //Debug
         Debug.Log("Amount to add: " + _amount);
 #endif
         INSTANCE.AttackersCoins += _amount;
@@ -74,7 +86,7 @@ public class CoinManager : MonoBehaviour
     private void PassiveIncome()
     {
         incomeTimer -= Time.deltaTime;
-        if (incomeTimer < 0)
+        if (incomeTimer < 0)//count down until ready, pays attacker in a passive way
         {
             AttackersCoins += passiveIncomeAmount;
             incomeTimer = passiveIncomeTime;
@@ -83,30 +95,31 @@ public class CoinManager : MonoBehaviour
 
     private void StealMoney()
     {
-        stealTimer -= Time.deltaTime;
-        if (isSTEAL)
+        if (isSteal)
         {
+            stealTimer -= Time.deltaTime;
             if (stealTimer <= 0)
             {
-                float _multiplier = STEALLEVEL * stealLevelMultiplier;
-                int _amount = Mathf.RoundToInt(baseStealAmount * _multiplier);
+                float _multiplier = stealLevel * stealLevelMultiplier;// tweak the multiplier level
+                int _amount = Mathf.RoundToInt(baseStealAmount * _multiplier);//add the multiplier and round amount to steal
                 DefendersCoins -= _amount;
                 AttackersCoins += _amount;
-                stealTimer = baseStealTime;
+                stealTimer = baseStealTime;//set timer back up
             }
         }
     }
 
     #endregion
 
+    //(Defender)
     #region --- DE FUNCTIONS ---
 
+    //function that is called once a minion is killed to calculate the reward
     public static void GainMinionPrize(int _minionLevel, int _minionPrize)
     {
         float _gain = 0;
-        _gain = _minionLevel * _minionPrize;
-        _gain *= INSTANCE.minionPrizeMultiplier;
-        int _amount = Mathf.RoundToInt(_gain);
+        _gain = _minionLevel * _minionPrize;//multiply by level to increase reward by status
+        int _amount = Mathf.RoundToInt(_gain * INSTANCE.minionPrizeMultiplier);//custum multiplier to tweak the final reward that is rounded to add to main counter
 #if UNITY_EDITOR
         Debug.Log("Killed a minion, Amount to gain: " + _amount);
 #endif
