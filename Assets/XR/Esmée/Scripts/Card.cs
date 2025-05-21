562d882268d8ba53e8400f0f163755b9547ec5b6
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2025-05-21 11:27:46 +0200

    minion spawning grid
    they also cant jump on eachother anymore and spawn in the floor

--- Assets/XR/Esmée/Scripts/Card.cs
+++ Assets/XR/Esmée/Scripts/Card.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     [SerializeField] private MinionScriptableObject prefabData;
+    [SerializeField] private float spawnLine;
+
     private bool hasBeenPlayed = false;
 
     public void OnPlay(Vector3 _spawnPosition)
@@ -10,23 +12,17 @@ public class Card : MonoBehaviour
         if (hasBeenPlayed) return;
         hasBeenPlayed = true;
 
+        if (_spawnPosition.z > spawnLine) _spawnPosition.z = spawnLine;
+        if (_spawnPosition.z < 4.3f) _spawnPosition.z = 4.3f;
+        if (_spawnPosition.x < -5.2f) _spawnPosition.x = -5.2f;
+        if (_spawnPosition.x > -3.3f) _spawnPosition.x = -3.3f;
 
-        if (Physics.Raycast(_spawnPosition, Vector3.down, out RaycastHit _hit))
         {
-            Vector3 _hitPos = _spawnPosition;
-
-            if (_hit.transform.CompareTag("PlayArea"))
+            if (prefabData != null && prefabData.MPrefab != null)
             {
-                if (prefabData != null && prefabData.MPrefab != null)
-                {
-                    Instantiate(prefabData.MPrefab, _spawnPosition, Quaternion.identity);
-                }
+                _spawnPosition.y = 0.1f; //dit werkt goed zodra we echte minion  model met pivit beneden hebben
+                Instantiate(prefabData.MPrefab, _spawnPosition, Quaternion.identity);
             }
-            else
-            {
-                Debug.Log("mowmow");
-            }
-
         }
     }
 }
