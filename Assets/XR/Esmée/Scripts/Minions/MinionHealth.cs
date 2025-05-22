5b226097787d88cc10f5eafd68a88a58fd661f8e
esmée veldhuizen <esmee.veldhuizen@outlook.com>
2025-05-22 14:14:23 +0200

    Health indicator for minions

--- Assets/XR/Esmée/Scripts/Minions/MinionHealth.cs
+++ Assets/XR/Esmée/Scripts/Minions/MinionHealth.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MinionHealth : MonoBehaviour
 {
     private float mHealth = 100;
+    private float maxHealth;
+    [SerializeField] private Image bar;
 
     private MinionScriptableObject MLevelData;
     private MinionScriptableObject MCostData;
 
+    private void Start()
+    {
+        maxHealth = mHealth;
+    }
     private void Update()
     {
         MDeath();
+        bar.fillAmount = Mathf.Clamp(mHealth / maxHealth, 0, 1);
     }
 
     public void TakeDamage(float _damageM)
@@ -21,7 +28,7 @@ public class MinionHealth : MonoBehaviour
     {
         if (mHealth <= 0)
         {
-            CoinManager.GainMinionPrize(MLevelData.MLevel, MCostData.MCost);
+            //CoinManager.GainMinionPrize(MLevelData.MLevel, MCostData.MCost);
             Destroy(gameObject);
         }
     }
